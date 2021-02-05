1fc87f23d8b089293b2a02156e24edae9fa13d9c
nastyalevk <levkovich.astya@gmail.com>
2021-02-05 17:31:19 +0300

    exception handler

---
+++ src/main/java/com/nastya/bookShop/config/RestExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.nastya.bookShop.config;
+
+import com.nastya.bookShop.model.error.ApiError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    protected ResponseEntity<Object> handleHttpClientError(
+            HttpClientErrorException ex) {
+        ApiError apiError = new ApiError(NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        logger.error("Server error: {}", ex.getMessage());
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+}
